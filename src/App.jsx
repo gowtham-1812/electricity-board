4c35104ebdda24c4cf4811e92966eb154f014638
Gowtham <138556855+gowtham-1812@users.noreply.github.com>
2025-04-02 20:49:58 +0530

    Update Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar.jsx";
-import Carousel from "./Carousel/Carousel_map.jsx";
+import Carousel from "./Carousel/Carousel_2.jsx";
 import "./App.css";
-import BillCalculator from "./BillCalculator.jsx";
 
 function App() {
     return(
         <div className="App" style={{ paddingTop: "70px" }}>
             <Navbar />
-            
-            {/* <div className="test">
-                <label>Name</label>
-                <TextBox onChange={setName} />
-
-                <label>About</label>
-                <TextArea onChange={setAbout} />
-
-                <p>Your Name: {name}</p>
-                <p>About: {about}</p>
-            </div> */}
-
-            <BillCalculator></BillCalculator>
+            <Carousel />
         </div>
     );
 }
