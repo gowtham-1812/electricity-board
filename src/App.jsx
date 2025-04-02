5aee8a23d3b0828f4686371c9b8c58f48a391bbc
Karthikeyan KS <86113017+KarthikeyanKS00747@users.noreply.github.com>
2025-04-02 22:46:00 +0530

    On branch main Your branch is behind 'origin/main' by 2 commits, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	modified:   src/App.jsx
    	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar2.jsx";
 import Carousel from "./Carousel/Carousel_2.jsx";
-import CurrentUpdates from "./CurrentUpdates.jsx";
-import Graphs from "./Graphs.jsx";
+import CurrentUpdates from "./Home/CurrentUpdates.jsx";
+import Graphs from "./Home/Graphs.jsx";
 import "./App.css";
+import {Route, Routes} from "react-router-dom";
+import BillCalculator from "./BillCalculator/BillCalculator.jsx";
 
 function App() {
     return(
         <div className="App" style={{ paddingTop: "70px" }}>
             <Navbar />
             <Carousel />
-            <div className="updates">
+            <Routes>
+                <Route path="/" element = { <Home/> } />
+                <Route path="/home" element = { <Home/> } />
+                <Route path="/bill-calculations" element = { <BillCalculator/> } />
+            </Routes>
+            {/* <div className="updates">
                 <CurrentUpdates />
                 <Graphs />
-            </div>
+            </div> */}
         </div>
     );
 }
